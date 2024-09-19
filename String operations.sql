81ff77fa92377092bf3f07ace83950e15c33ddf8
GeethaPachaiyappan <141610500+GeethaPachaiyappan@users.noreply.github.com>
2024-09-19 14:48:31 +0530

    Update String operations

--- String operations.sql
+++ String operations.sql
@@ -13,3 +13,5 @@ select distinct(city) from station where UPPER(left(city,1)) in ('A','E','I','O'
 select distinct(city) from station where LOWER(left(city,1)) NOT IN ('a','e','i','o','u');
 
 select name from students where marks > 75 order by right(name,3),id;
+
+select * from students;
